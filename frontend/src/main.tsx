ff3781d6188b6a769d5fffa1ac3d1e477d53b00e
bishalhota <bishalhota264@gmail.com>
2026-04-08 18:35:18 +0530

    Frontend for google auth completed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 
 export const authService = "http://localhost:5000";
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="500323250919-0cj6enb6plmc4j475q1mb99svupuaohl.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -10,9 +10,10 @@ dotenv.config();
 const app = express();
 
 
-
 app.use(cors());
+
 app.use(express.json());
+
 
 app.use("/api/auth",authRoutes);
 
